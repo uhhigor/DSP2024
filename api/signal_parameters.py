998220a43cd11198540cc4d215c8dd1121148bcb
MaciejPerdaszek <maciej.44@wp.pl>
2024-05-13 21:19:46 +0200

    Better display

--- api/signal_parameters.py
+++ api/signal_parameters.py
@@ -60,3 +60,40 @@ def continuous_variance(y_values):
 
 def continuous_effective_value(y_values):
     return math.sqrt(average_power(y_values))
+
+
+def mean_square_error(y_values, y_values2):
+    samples = len(y_values)
+    n = 0
+    sumxt = 0
+    while n < samples:
+        if math.isnan(y_values[n]) or math.isnan(y_values2[n]):
+            n += 1
+            continue
+        sumxt += (y_values[n] - y_values2[n]) ** 2
+        n += 1
+
+    return sumxt / samples
+
+
+def signal_to_noise_ratio(y_values, y_values2):
+    mse = mean_square_error(y_values, y_values2)
+    if mse == 0:
+        return float('inf')
+    return 10 * math.log10(average_power(y_values) / mse)
+
+
+def maximum_difference(y_values, y_values2):
+    samples = len(y_values)
+    n = 0
+    max_diff = 0
+    while n < samples:
+        if math.isnan(y_values[n]) or math.isnan(y_values2[n]):
+            n += 1
+            continue
+        diff = math.fabs(y_values[n] - y_values2[n])
+        if diff > max_diff:
+            max_diff = diff
+        n += 1
+
+    return max_diff
--- gui/signal_info_frame.py
+++ gui/signal_info_frame.py
@@ -10,8 +10,13 @@ from api import signal_conversion
 
 class SignalInfoFrame:
     def __init__(self, master, y_values, t_values, frequency, start_time, num_samples):
+        self.selected_reconstruction = None
+        self.selected_quantization = None
+        self.number_of_samples_entry = None
+        self.number_of_samples = None
         self.master = master
         self.frame = ttk.Frame(self.master, padding="10")
+        self.init_y_values = y_values
         self.y_values = y_values
         self.t_values = t_values
         self.frequency = frequency
@@ -28,6 +33,13 @@ class SignalInfoFrame:
         self.variance_label_value = ttk.Label(self.frame, text="")
         self.effective_label = ttk.Label(self.frame, text="Wartość skuteczna sygnału: ")
         self.effective_label_value = ttk.Label(self.frame, text="")
+
+        self.mse_label = ttk.Label(self.frame, text="Błąd średniokwadratowy: ")
+        self.mse_label_value = ttk.Label(self.frame, text="")
+        self.snr_label = ttk.Label(self.frame, text="Stosunek sygnał-szum: ")
+        self.snr_label_value = ttk.Label(self.frame, text="")
+        self.md_label = ttk.Label(self.frame, text="Maksymalna różnica: ")
+        self.md_label_value = ttk.Label(self.frame, text="")
 
         self.save_button = ttk.Button(self.frame, text="Zapisz do pliku", command=self.save_to_file)
         self.file_name_label = ttk.Label(self.frame, text="Nazwa pliku: ")
@@ -54,20 +66,40 @@ class SignalInfoFrame:
         self.file_name_entry.grid(column=1, row=8)
         self.file_name_entry.insert(0, "signal")
 
-    def show(self, y_values, t_values, type="plot"):
+        self.mse_label.grid(column=2, row=6)
+        self.mse_label_value.grid(column=3, row=6)
+        self.snr_label.grid(column=2, row=7)
+        self.snr_label_value.grid(column=3, row=7)
+        self.md_label.grid(column=2, row=8)
+        self.md_label_value.grid(column=3, row=8)
+
+    def show(self, y_values, t_values, type="plot", color="blue"):
         self.avg_label_value.config(text=str(round(signal_parameters.average_value(y_values), 3)))
         self.avg_abs_label_value.config(text=str(round(signal_parameters.average_value_absolute(y_values), 3)))
         self.avg_power_label_value.config(text=str(round(signal_parameters.average_power(y_values), 3)))
         self.variance_label_value.config(text=str(round(signal_parameters.continuous_variance(y_values), 3)))
         self.effective_label_value.config(text=str(round(signal_parameters.continuous_effective_value(y_values), 3)))
         plot = plt.figure()
+        ax1 = plot.add_subplot(211)
+        ax2 = plot.add_subplot(212)
         if type == "plot":
-            plt.plot(t_values, y_values)
+            ax1.plot(t_values, y_values, color=color)
+            ax2.hist(y_values, bins=10, edgecolor='black', color='gray')
         elif type == "scatter":
-            plt.scatter(t_values, y_values, 5)
+            display_y = []
+            display_t = []
+            number_of_samples = self.num_samples / int(self.number_of_samples.get())
+            for i in range(0, len(y_values), int(number_of_samples)):
+                display_y.append(y_values[i])
+                display_t.append(t_values[i])
+            ax1.scatter(display_t, display_y, 5, color=color)
+            ax2.hist(display_y, bins=10, edgecolor='black',  color='gray')
         canvas = FigureCanvasTkAgg(plot, master=self.frame)
         canvas.draw()
-        canvas.get_tk_widget().grid(column=0, row=6, columnspan=2)
+        if color == "red":
+            canvas.get_tk_widget().grid(column=2, row=9, columnspan=2)
+        else:
+            canvas.get_tk_widget().grid(column=0, row=9, columnspan=2)
         self.frame.grid(column=0, row=0, columnspan=2, rowspan=6)
 
     def show_buttons(self):
@@ -89,27 +121,39 @@ class SignalInfoFrame:
 
         ttk.Button(self.frame, text="Wykonaj rekonstrukcje", command=self.execute_reconstruction).grid(column=2, row=4)
 
+    def calculate_measurements(self, y_values, y1_values):
+        self.mse_label_value.config(text=str(round(signal_parameters.mean_square_error(y_values, y1_values), 3)))
+        self.snr_label_value.config(text=str(round(signal_parameters.signal_to_noise_ratio(y_values, y1_values), 3)))
+        self.md_label_value.config(text=str(round(signal_parameters.maximum_difference(y_values, y1_values), 3)))
+
     def execute_sampling(self):
-        sampled_y = signal_conversion.conversion_sampling(self.y_values, int(self.number_of_samples.get()))
+        sampled_y = signal_conversion.conversion_sampling(self.init_y_values, int(self.number_of_samples.get()))
         self.y_values = sampled_y
-        self.show(self.y_values, self.t_values, "scatter")
+        self.show(self.init_y_values, self.t_values)
+        self.show(self.y_values, self.t_values, "scatter", "red")
+        self.calculate_measurements(self.init_y_values, self.y_values)
 
     def execute_quantization(self):
         quantized = signal_conversion.uniform_quantization(self.y_values)
         self.y_values = quantized
-        self.show(self.y_values, self.t_values, "scatter")
-        pass
+        self.show(self.init_y_values, self.t_values)
+        self.show(self.y_values, self.t_values, "plot", "red")
+        self.calculate_measurements(self.init_y_values, self.y_values)
 
     def execute_reconstruction(self):
         reconstructed = None
         if self.selected_reconstruction.get() == "Ekstrapolacja zerowego rzędu":
-            reconstructed = signal_conversion.zero_order_extrapolation(self.y_values, int(self.number_of_samples.get()))
+            reconstructed = signal_conversion.zero_order_extrapolation(self.y_values,
+                                                                       int(self.number_of_samples.get()))
         elif self.selected_reconstruction.get() == "Interpolacja pierwszego rzędu":
-            reconstructed = signal_conversion.first_order_extrapolation(self.y_values, int(self.number_of_samples.get()))
+            reconstructed = signal_conversion.first_order_extrapolation(self.y_values,
+                                                                        int(self.number_of_samples.get()))
         elif self.selected_reconstruction.get() == "Rekonstrukcja w oparciu o funkcje sinc":
             reconstructed = signal_conversion.sinc_extrapolation(self.y_values, int(self.number_of_samples.get()))
         self.y_values = reconstructed
-        self.show(self.y_values, self.t_values)
+        self.show(self.init_y_values, self.t_values)
+        self.show(self.y_values, self.t_values, "plot", "red")
+        self.calculate_measurements(self.init_y_values, self.y_values)
 
     def save_to_file(self):
         params = {
